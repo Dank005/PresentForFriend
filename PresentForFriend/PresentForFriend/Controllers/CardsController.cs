df1f25c79c44e542a4f40aac8f14ed4039c20082
Dank005 <daniil.yushkov2000@mail.ru>
2021-11-11 17:13:37 +0500

    do views of pages

---
+++ PresentForFriend/PresentForFriend/Controllers/CardsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentForFriend.Controllers
+{
+    public class CardsController : Controller
+    {
+        [Route("cards/{id}")]
+        public IActionResult Index(int id) //показывает в View/Cards файл Index
+        {
+            ViewBag.ItemId = id;
+
+            return View();
+        }
+    }
+}
